40a9ad6a5cdd272518a12568de35818778f8aaac
ahmadms2020 <najyms@gmail.com>
2021-09-04 18:44:44 +0300

    s

---
+++ AdministratorApp/AdministratorApp/Classes/ApiClasses/ErrorClass.cs
@@ -0,0 +1,124 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AdministratorApp.ApiClasses
+{
+    public class ErrorClass
+    {
+
+        public int errorId { get; set; }
+        public string num { get; set; }
+        public string msg { get; set; }
+        public string stackTrace { get; set; }
+        public string targetSite { get; set; }
+        public Nullable<System.DateTime> createDate { get; set; }
+        public Nullable<int> createUserId { get; set; }
+
+        private string urimainpath = "errorController/";
+        /// <summary>
+        /// ///////////////////////////////////////
+        /// </summary>
+        /// <returns></returns>
+        /// 
+
+        public async Task<List<ErrorClass>> GetAll()
+        {
+            List<ErrorClass> memberships = null;
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            using (var client = new HttpClient())
+            {
+                Uri uri = new Uri(Global.APIUri + urimainpath + "GetAll");
+                response = await ApiConnect.ApiGetConnect(uri);
+
+                response = await ApiConnect.ApiGetConnect(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+
+                    memberships = JsonConvert.DeserializeObject<List<ErrorClass>>(jsonString);
+
+                    return memberships;
+                }
+                else //web api sent error response 
+                {
+                    memberships = new List<ErrorClass>();
+                }
+                return memberships;
+            }
+
+        }
+
+        public async Task<string> Save(ErrorClass obj)
+        {
+            var myContent = JsonConvert.SerializeObject(obj);
+            myContent = HttpUtility.UrlEncode(myContent);
+            Uri uri = new Uri(Global.APIUri + urimainpath + "Save?Object=" + myContent);
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            response = await ApiConnect.ApiPostConnect(uri);
+            using (var client = new HttpClient())
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    message = JsonConvert.DeserializeObject<string>(message);
+                    return message;
+                }
+                return "";
+            }
+        }
+
+        public async Task<ErrorClass> GetByID(int errorId)
+        {
+            ErrorClass obj = new ErrorClass();
+          
+            HttpResponseMessage response = new HttpResponseMessage();
+            using (var client = new HttpClient())
+            {
+
+                Uri uri = new Uri(Global.APIUri + urimainpath + "GetByID?errorId=" + errorId);
+
+                response = await ApiConnect.ApiGetConnect(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                   var jsonString = await response.Content.ReadAsStringAsync();
+                    obj = JsonConvert.DeserializeObject<ErrorClass>(jsonString);
+                    return obj;
+                }
+
+                return obj;
+            }
+        }
+
+        public async Task<string> Delete(int errorId, int userId, bool final)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            using (var client = new HttpClient())
+            {
+                Uri uri = new Uri(Global.APIUri + urimainpath + "Delete?errorId=" + errorId + "&userId=" + userId + "&final=" + final);
+                response = await ApiConnect.ApiPostConnect(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    message = JsonConvert.DeserializeObject<string>(message);
+                    return message;
+                }
+                return "";
+            }
+        }
+
+
+
+
+
+    }
+}
