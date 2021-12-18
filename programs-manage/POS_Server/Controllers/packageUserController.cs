30ef35a83c4d8942a802d2a196ee0526f118c848
ahmadms2020 <najyms@gmail.com>
2021-12-18 17:59:45 +0200

    a

--- programs-manage/POS_Server/Controllers/packageUserController.cs
+++ programs-manage/POS_Server/Controllers/packageUserController.cs
@@ -1024,5 +1024,50 @@ namespace Programs_Server.Controllers
             }
         }
 
+
+        [HttpPost]
+        [Route("GetLastNumOfCode")]
+        public string GetLastNumOfCode(string token)
+        {
+            token = TokenManager.readToken(HttpContext.Current.Request);
+            string message = "";
+            var strP = TokenManager.GetPrincipal(token);
+            if (strP != "0") //invalid authorization
+            {
+                return TokenManager.GenerateToken(strP);
+            }
+            else
+            {
+                string type = "";
+                IEnumerable<Claim> claims = TokenManager.getTokenClaims(token);
+                foreach (Claim c in claims)
+                {
+                    if (c.Type == "type")
+                    {
+                        type = c.Value;
+                    }
+                }
+                List<string> numberList;
+                int lastNum = 0;
+                using (incprogramsdbEntities entity = new incprogramsdbEntities())
+                {
+                    numberList = entity.packageUser.Where(b => b.packageNumber.Contains(type + "-")).Select(b => b.packageNumber).ToList();
+
+                    for (int i = 0; i < numberList.Count; i++)
+                    {
+                        string packageNumber = numberList[i];
+                        string s = packageNumber.Substring(packageNumber.LastIndexOf("-") + 1);
+                        numberList[i] = s;
+                    }
+                    if (numberList.Count > 0)
+                    {
+                        numberList.Sort();
+                        lastNum = int.Parse(numberList[numberList.Count - 1]);
+                    }
+                }
+                message = lastNum.ToString();
+                return TokenManager.GenerateToken(message);
+            }
+        }
     }
 }
