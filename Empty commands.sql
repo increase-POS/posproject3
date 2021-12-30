502b4e7855d8d7fa747f0637f405722397c08158
mnasani <mnasani79@gmail.com>
2021-12-30 20:10:56 +0200

    empty cmnd

--- Empty commands.sql
+++ Empty commands.sql
@@ -1,3 +1,13 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
+USE [incprogramsdb]
+GO
 delete
+  FROM [incprogramsdb].[dbo].[posSerials]
+
+  delete
+  FROM [incprogramsdb].[dbo].[payOp]
+
+  delete
+  FROM [incprogramsdb].[dbo].[packageUser]
+
+  delete
   FROM [incprogramsdb].[dbo].[countryPackageDate]
