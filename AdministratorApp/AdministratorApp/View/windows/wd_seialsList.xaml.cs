9b320831ad9b1e57432a292033da4040b2d0b584
yasmineNjr <82780508+yasmineNjr@users.noreply.github.com>
2022-03-08 14:58:43 +0200

    save serial list

--- AdministratorApp/AdministratorApp/View/windows/wd_seialsList.xaml.cs
+++ AdministratorApp/AdministratorApp/View/windows/wd_seialsList.xaml.cs
@@ -46,11 +46,8 @@ namespace AdministratorApp.View.windows
         Packages pModel = new Packages();
         int isActiveCount = 0;
 
-        //pagination
-        //int pageIndex = 0 ;
-        int pageIndexCurrent = 0, pageCount = 0;
-        List<Button> btnList = new List<Button>();
         int itemsPerPage = 20;
+        private int pageCount;
 
         public wd_seialsList()
         {
@@ -70,6 +67,7 @@ namespace AdministratorApp.View.windows
             {
                 HelpClass.StartAwait(grid_main);
             btns = new Button[] { btn_firstPage, btn_prevPage, btn_activePage, btn_nextPage, btn_lastPage };
+
             #region translate
             if (MainWindow.lang.Equals("en"))
             {
@@ -84,7 +82,7 @@ namespace AdministratorApp.View.windows
             translat();
             #endregion
 
-            #region fill user type
+            #region fill page count combo
             var typelist = new[] {
                 new { Text = "20"    , Value = "20" },
                 new { Text = "50"    , Value = "50" },
@@ -105,31 +103,9 @@ namespace AdministratorApp.View.windows
 
             cb_itemPerPage.SelectedIndex = 0;
 
-
             Tb_search_TextChanged(tb_search, null);
-                /*
-                    RefreshView();
-                    */
-                #region pagination
-                /*
-                    btnList.Add(btn_firstPage);
-                    btnList.Add(btn_prevPage);
-                    btnList.Add(btn_curPage);
-                    btnList.Add(btn_nextPage);
-                    btnList.Add(btn_lastPage);
-
-                    pageIndex = 1;
-                    pageIndexCurrent = 1;
-                    pageCount = posSerialsQuery.Count() / itemsPerPage;
-
-                    if (pageCount == 1)
-                    { btn_curPage.IsEnabled = false; btn_nextPage.IsEnabled = false; btn_lastPage.IsEnabled = false; }
-                    else if (pageCount == 2)
-                    { btn_nextPage.IsEnabled = false; }
-                    */
-                #endregion
-
-                HelpClass.EndAwait(grid_main);
+             
+            HelpClass.EndAwait(grid_main);
             }
             catch (Exception ex)
             {
@@ -138,193 +114,6 @@ namespace AdministratorApp.View.windows
             }
         }
         
-        #region pagination
-        /*
-        private void Btn_firstPage_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                pageIndex = 1;
-                (sender as Button).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#178DD2"));
-                clearOtherButtons(sender);
-                RefreshView();
-            }
-            catch (Exception ex)
-            {
-                HelpClass.ExceptionMessage(ex, this);
-            }
-        }
-
-        private void Btn_prevPage_Click(object sender, RoutedEventArgs e)
-        {
-            //try
-            //{
-                pageIndex = Convert.ToInt32((sender as Button).Content);
-
-                if (pageIndex != 1)
-                {
-                    btn_curPage.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#178DD2"));
-                    clearOtherButtons(btn_curPage);
-                    RefreshViewButtons();
-                }
-                else
-                {
-                    (sender as Button).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#178DD2"));
-                    clearOtherButtons(sender);
-                }
-
-                RefreshView();
-            //}
-            //catch (Exception ex)
-            //{
-            //    HelpClass.ExceptionMessage(ex, this);
-            //}
-        }
-
-        private void Btn_curPage_Click(object sender, RoutedEventArgs e)
-        {
-            //try
-            //{
-                pageIndex = Convert.ToInt32((sender as Button).Content);
-                (sender as Button).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#178DD2"));
-                clearOtherButtons(sender);
-                if (!((pageIndex == 2) || (pageIndex == pageCount-1)))
-                {
-                    RefreshViewButtons();
-                }
-                RefreshView();
-            //}
-            //catch (Exception ex)
-            //{
-            //    HelpClass.ExceptionMessage(ex, this);
-            //}
-        }
-
-        private void Btn_nextPage_Click(object sender, RoutedEventArgs e)
-        {
-            //try
-            //{
-                
-                pageIndex = Convert.ToInt32((sender as Button).Content);
-               
-                if (pageIndex != pageCount)
-                {
-                    btn_curPage.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#178DD2"));
-                    clearOtherButtons(btn_curPage);
-                    RefreshViewButtons();
-                }
-                else
-                {
-                    (sender as Button).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#178DD2"));
-                    clearOtherButtons(sender);
-                }
-                
-                RefreshView();
-            //}
-            //catch (Exception ex)
-            //{
-            //    HelpClass.ExceptionMessage(ex, this);
-            //}
-        }
-
-        private void Btn_lastPage_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                pageIndex = pageCount;
-                (sender as Button).Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#178DD2"));
-                clearOtherButtons(sender);
-                RefreshView();
-            }
-            catch (Exception ex)
-            {
-                HelpClass.ExceptionMessage(ex, this);
-            }
-        }
-        private void clearOtherButtons(object sender)
-        {
-            foreach(var b in btnList)
-            {
-                if (b.Name != (sender as Button).Name)
-                {
-                    b.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#DFDFDF"));
-                    b.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#686868"));
-                }
-            }
-        }
-
-        private void RefreshViewButtons()
-        {
-           
-            btn_prevPage.Content = pageIndex -1;
-            btn_curPage.Content = pageIndex;
-            btn_nextPage.Content = pageIndex + 1;
-          
-          
-        }
-        */
-
-        private void Number_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {//only  digits
-            try
-            {
-                TextBox textBox = sender as TextBox;
-                HelpClass.InputJustNumber(ref textBox);
-                Regex regex = new Regex("[^0-9]+");
-                e.Handled = regex.IsMatch(e.Text);
-            }
-            catch (Exception ex)
-            {
-                HelpClass.ExceptionMessage(ex, this);
-            }
-        }
-
-        private void Spaces_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            try
-            {
-                e.Handled = e.Key == Key.Space;
-            }
-            catch (Exception ex)
-            {
-                HelpClass.ExceptionMessage(ex, this);
-            }
-        }
-
-        private void Tb_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            try
-            {
-                /*
-                if (int.Parse(tb_page.Text) <= pageCount)
-                {
-                    pageIndex = int.Parse(tb_page.Text);
-                    ////btn_curPage.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#178DD2"));
-                    ////clearOtherButtons(btn_curPage);
-                    ////if (pageIndex == 1) Btn_firstPage_Click(btn_firstPage, null);
-                    ////else if (pageIndex == pageCount) Btn_lastPage_Click(btn_lastPage, null);
-                    ////else
-                    //    Btn_curPage_Click(btn_curPage , null);
-                    ////if ((pageIndex < pageCount-2 )&&(pageIndex >2))
-                    ////RefreshViewButtons();
-                    ////RefreshView();
-                    ///
-                    btn_curPage.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#178DD2"));
-                    clearOtherButtons(sender);
-                    if (!((pageIndex == 2) || (pageIndex == pageCount - 1)))
-                    {
-                        RefreshViewButtons();
-                    }
-                    RefreshView();
-                }
-                */
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
-        #endregion
         #region save - search - close - refresh
         private void Btn_colse_Click(object sender, RoutedEventArgs e)
         {//close
@@ -337,19 +126,13 @@ namespace AdministratorApp.View.windows
             {
                 HelpClass.StartAwait(grid_main);
 
-                //List<PosSerials> posSerialsNew = new List<PosSerials>();
-                //foreach (PosSerials row in dg_serials.Items)
-                //{
-                //    if (row.isActive == 1)
-                //        posSerialsNew.Add(row);
-                //}
-                //posSerialsNew = posSerials.ToList();
-
                 if ((isActiveCount <= p.posCount) || (p.posCount == -1))
                 {
                    List< PosSerialsUpdate> uplist = new List<PosSerialsUpdate>();
-
-                    foreach (PosSerials srow in posSerials)
+                    //posSerialsQueryPage
+                   
+                    int x = posSerialsQueryPage.Count();
+                    foreach (PosSerials srow in posSerialsQuery)
                     {
                         PosSerialsUpdate uprow = new PosSerialsUpdate();
                         uprow.serialId =srow.serialId ;
@@ -389,9 +172,7 @@ namespace AdministratorApp.View.windows
                 posSerialsQuery = posSerialsQuery.Where(s => s.serial.Contains(txtSearch)
                 || (s.posName    != null ? s.posName.Contains(txtSearch)    : false)
                 || (s.branchName != null ? s.branchName.Contains(txtSearch) : false));
-                /*
-                RefreshView();
-                */
+
                 refreshGrid();
               }
             catch (Exception ex)
@@ -424,26 +205,6 @@ namespace AdministratorApp.View.windows
         #endregion
 
         #region events
-        //private void dg_serials_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
-        //{
-        //    if (e.EditAction == DataGridEditAction.Commit)
-        //    {
-        //        var column = e.Column as DataGridBoundColumn;
-        //        if (column != null)
-        //        {
-        //            var bindingPath = (column.Binding as Binding).Path.Path;
-        //            if (bindingPath == "isActive")
-        //            {
-        //                int rowIndex = e.Row.GetIndex();
-        //                var el = e.EditingElement as CheckBox;
-        //                MessageBox.Show(el.IsChecked.ToString());
-        //                // rowIndex has the row index
-        //                // bindingPath has the column's binding
-        //                // el.Text has the new, user-entered value
-        //            }
-        //        }
-        //    }
-        //}
         private void HandleKeyPress(object sender, KeyEventArgs e)
         {
             try
@@ -520,10 +281,6 @@ namespace AdministratorApp.View.windows
 
             chk_allSerials.Content = MainWindow.resourcemanager.GetString("trSelectAll");
 
-            //tt_first.Content = MainWindow.resourcemanager.GetString("trFirst");
-            //tt_last.Content = MainWindow.resourcemanager.GetString("trLast");
-            //MaterialDesignThemes.Wpf.HintAssist.SetHint(tb_page, "...");
-
         }
 
         async Task<IEnumerable<PosSerials>> RefreshList()
@@ -532,35 +289,6 @@ namespace AdministratorApp.View.windows
             return posSerials;
         }
         IEnumerable<PosSerials> posSerialsQueryPage = new List<PosSerials>();
-        /*
-        private void RefreshView()
-        {
-            posSerialsQueryPage = new List<PosSerials>();
-           
-            int index = (pageIndex - 1) * itemsPerPage;
-            int i = 0;
-
-            while (i < itemsPerPage)
-            {
-                i++;
-                try
-                {
-                    PosSerials serial = posSerialsQuery.ToList()[index];
-                    posSerialsQueryPage.Add(serial);
-                    index++;
-                }
-                catch { }
-            }
-
-            if (posSerialsQueryPage.Count > 0)
-            {
-                dg_serials.ItemsSource = posSerialsQueryPage;
-                //txt_count.Text = dg_serials.Items.Count.ToString();
-                txt_count.Text = MainWindow.resourcemanager.GetString("trPage") + " " + pageIndex.ToString() + " " +
-                                 MainWindow.resourcemanager.GetString("trOf")   + " " + pageCount.ToString();
-            }
-        }
-        */
         #endregion
         #region Pagination Y
         Pagination pagination = new Pagination();
@@ -571,6 +299,15 @@ namespace AdministratorApp.View.windows
             if (btns is null)
                 btns = new Button[] { btn_firstPage, btn_prevPage, btn_activePage, btn_nextPage, btn_lastPage };
             posSerialsQueryPage = pagination.refrishPagination(posSerialsQuery, pageIndex, btns, itemsPerPage);
+            foreach (PosSerials srow in posSerialsQuery)
+            {
+                foreach (PosSerials grow in posSerialsQueryPage)
+                {
+                    if (grow.serialId == srow.serialId)
+
+                        srow.isActive = grow.isActive;
+                }
+            }
             dg_serials.ItemsSource = posSerialsQueryPage;
 
         }
@@ -581,8 +318,6 @@ namespace AdministratorApp.View.windows
             {
                 if (sender != null)
                     HelpClass.StartAwait(grid_main);
-
-               
 
                 if (tb_pageNumberSearch.Text.Equals(""))
                 {
@@ -893,20 +628,10 @@ namespace AdministratorApp.View.windows
                     if ((isActiveCount <= p.posCount) || (p.posCount == -1))
                     {
                         s.isActive = 1;
-                    dg_serials.ItemsSource = posSerialsQueryPage;
-                    dg_serials.Items.Refresh();
+                        dg_serials.ItemsSource = posSerialsQueryPage;
+                        dg_serials.Items.Refresh();
                     }
                 }
-                //cb_itemPerPage.IsEnabled = false;
-                //grid_pagination.IsEnabled = false;
-                //dg_serials.ItemsSource = posSerialsQuery;
-
-                //itemsPerPage = posSerialsQuery.Count();
-                //pageIndex = 1;
-                //Tb_search_TextChanged(tb_search, null);
-
-                //RefreshView();
-                //dg_serials.Items.Refresh();
             }
             catch (Exception ex)
             {
@@ -918,24 +643,14 @@ namespace AdministratorApp.View.windows
         {
             try
             {
-
-            chk_allSerials.Content = MainWindow.resourcemanager.GetString("trSelectAll");
-            //cb_itemPerPage.IsEnabled = true;
-            //grid_pagination.IsEnabled = true;
-            //dg_serials.ItemsSource = posSerialsQuery;
-
-            //itemsPerPage = int.Parse(cb_itemPerPage.SelectedValue.ToString());
-            //pageCount = posSerialsQuery.Count() / itemsPerPage;
-            //pageIndex = 1;
-            //Tb_search_TextChanged(tb_search, null);
+                chk_allSerials.Content = MainWindow.resourcemanager.GetString("trSelectAll");
+           
                 foreach (var s in posSerialsQueryPage)
                 {
                     s.isActive = 0;
-                dg_serials.ItemsSource = posSerialsQueryPage;
-                dg_serials.Items.Refresh();
-            }
-            //RefreshView();
-            //dg_serials.Items.Refresh();
+                    dg_serials.ItemsSource = posSerialsQueryPage;
+                    dg_serials.Items.Refresh();
+                }
             }
             catch (Exception ex)
             {
